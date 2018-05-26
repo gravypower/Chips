3156f97361e78250ab5cae272d2b9bb31c8dd11c
gravypower <aaron@gravypower.net>
2018-05-26 22:32:38 +1000

    Started to add in DryIoc

---
+++ Chips.DependencyInjection.DryIoc/DryIocBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using DryIoc;
+
+namespace Chips.DependencyInjection.DryIoc
+{
+    public class DryIocBootstrapper : Bootstrapper<Container>
+    {
+
+        public DryIocBootstrapper(IEnumerable<Assembly> assembls) : base(assembls)
+        {
+        }
+
+        protected override Container CreateContainer()
+        {
+            return new Container();
+        }
+
+        protected override void VerifyContainer()
+        {
+
+            Container.Validate();
+        }
+    }
+}
---
+++ Chips.Sitecore.ApplicationContainer.Dryioc/InitialiseControllerFactory.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using DryIoc;
+
+namespace Chips.Sitecore.ApplicationContainer.Dryioc
+{
+    public class InitialiseControllerFactory: InitialiseControllerFactory<Container>
+    {
+        protected override ControllerFactory<Container> GetControllerFactory(IControllerFactory innerFactory)
+        {
+            return new ControllerFactory(innerFactory);
+        }
+    }
+}
