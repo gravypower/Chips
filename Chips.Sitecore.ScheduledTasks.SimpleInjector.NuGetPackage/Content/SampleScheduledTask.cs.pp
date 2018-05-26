9af6d38e0061ec1898d409fa82b594f152ac0238
gravypower <aaron@gravypower.net>
2018-05-26 22:03:54 +1000

    Added nuget package for Shedule Task Container adaptor

---
+++ Chips.Sitecore.ScheduledTasks.SimpleInjector.NuGetPackage/Content/SampleScheduledTask.cs.pp
@@ -0,0 +1,12 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Tasks;
+
+namespace $rootnamespace$
+{
+    public class SampleScheduledTask:IScheduleTask
+    {
+        public void Execute(Item[] items, CommandItem command, ScheduleItem schedule)
+        {
+        }
+    }
+}
