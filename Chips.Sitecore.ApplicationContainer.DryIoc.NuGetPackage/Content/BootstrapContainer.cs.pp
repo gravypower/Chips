5bf2be9b87eaa4b1e7bb42c8e3361ae81015f354
gravypower <aaron@gravypower.net>
2018-05-27 10:44:38 +1000

    Created NuGet projects for DryIoc implementation

---
+++ Chips.Sitecore.ApplicationContainer.DryIoc.NuGetPackage/Content/BootstrapContainer.cs.pp
@@ -0,0 +1,13 @@
+﻿using System;
+using Chips.DependencyInjection;
+using SimpleInjector;
+
+namespace $rootnamespace$
+{
+    public class BootstrapContainer : IBootstrap<Container>
+    {
+        public void Bootstrap(Container container)
+        {
+        }
+    }
+}
